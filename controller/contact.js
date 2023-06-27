05a6f9f31955a983863e3ce0d3260f77b8465d1c
Manish Khandar <manishkhandar13@gmail.com>
2023-06-27 14:53:41 +0530

    Implemeting /identify endpoint

--- controller/contact.js
+++ controller/contact.js
@@ -1,6 +1,7 @@
 const ContactModel = require('../models/contactModel')
 const { Sequelize, Op } = require('sequelize');
 const { v4: uuidv4 } = require('uuid');
+const _ = require('lodash')
 
 
 function generateUniqueInt() {
@@ -68,6 +69,55 @@ exports.createContact = async (req, res) => {
 		return res.status(200).json({ message: 'Contact added successfully' });
 	} catch (error) {
 		console.error('Error adding contact:', error);
+		return res.status(500).json({ error: 'Internal server error' });
+	}
+};
+
+
+exports.identifyContact = async (req, res) => {
+	try {
+		const phoneNumber = req.body.phoneNumber || null;
+		const email = req.body.email || null
+		let filter, order
+		order = [['createdat', 'ASC']]
+
+		//primary contact
+		filter = {
+			linkprecedence: 'primary',
+			[Op.or]: [
+				{ email },
+				{ phonenumber :phoneNumber }
+			]
+		}
+		const primaryContact = await ContactModel.get(filter, order);
+
+		if(primaryContact.length === 0) {
+			return res.status(404).json({message : "No order details found for this contacts"})
+		}
+
+		//Linked contacts
+		filter = {
+			linkedid: primaryContact[0].id,
+		}
+		const LinkedContacts = await ContactModel.get(filter, order);
+
+		const emails = _.uniq(LinkedContacts.map(obj => obj.email));
+		const phoneNumbers = _.uniq(LinkedContacts.map(obj => obj.phonenumber));
+		const secondaryIds = _.uniq(LinkedContacts.map(obj => obj.id));
+
+		let response =
+		{
+			"contact":{
+				"primaryContatctId": primaryContact[0].id,
+				"emails": _.union( [primaryContact[0].email] ,emails),
+				"phoneNumbers": _.union ([primaryContact[0].phonenumber], phoneNumbers),
+				"secondaryContactIds": secondaryIds
+			}
+		}
+
+		return res.status(200).json(response);
+	} catch (error) {
+		console.error('Error identifying contact(s):', error);
 		return res.status(500).json({ error: 'Internal server error' });
 	}
 };
